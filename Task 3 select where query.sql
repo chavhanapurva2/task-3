d836f112bf719ebd5be62e73df2104561c8f933c
chavhanapurva2 <chavhanapurva2@gmail.com>
2024-06-26 18:41:51 +0530

    task 3 commit

---
+++ Task 3 select where query.sql
@@ -0,0 +1,21 @@
+select * from product
+
+select emp_id from product
+
+select date_dob from product
+
+select product_id,product,month,rate,qty from product
+
+select * from product where rate < 400
+
+select * from product where product ='RAM'
+
+select * from product where month = 'Jan'
+
+select * from product where qty > 6 and qty < 15 
+
+select * from product where name = 'Apurva Chavhan'
+
+select * from product where product_id < 50 and product_id > 10
+
+select * from product where date_dob = '2024-05-04'
